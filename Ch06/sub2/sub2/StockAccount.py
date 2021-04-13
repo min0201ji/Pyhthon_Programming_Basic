fc31a117b14316d3b1b03a60b6b69ef3a2430b46
min0201ji <82108049+min0201ji@users.noreply.github.com>
2021-04-13 21:33:23 +0900

    Add files via upload

---
+++ Ch06/sub2/sub2/StockAccount.py
@@ -0,0 +1,23 @@
+from Ch06.sub1.Account import Account
+
+class StockAccount(Account):
+    def __init__(self, bank, id, name, money, stock, amount, price):
+        # 부모클래스(Account)생성자 실행, 자식클래스(StockAccount)
+        super().__init__(bank, id, name, money)
+        self.stock = stock
+        self.amount = amount
+        self.price = price
+        
+    def sell(self, amount, price):
+        self._money += (self.amount * self.price)
+        self.amount -= amount
+        
+    def buy(self, amount, price):
+        self._money -= (self.amount * self.price)
+        self.amount += amount
+        
+    def show(self):
+        super().show()
+        print('주식종목 :', self.stock)
+        print('주식수량 :', self.amount)
+        print('주식가격 :', self.price)
