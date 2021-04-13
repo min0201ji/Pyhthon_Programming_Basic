c699e02fb6ecb6b759ee4d7487b9f0df545d5444
min0201ji <82108049+min0201ji@users.noreply.github.com>
2021-04-13 21:33:07 +0900

    Add files via upload

--- Ch06/6_3_Inherit.py
+++ Ch06/6_3_Inherit.py
@@ -8,11 +8,11 @@ from Ch06.sub2.StockAccount import StockAccount
 from Ch06.sub2.SmartPhone import SmartPhone
 
 
-kb = StockAccount('KB증권', '101-12-1222', '김유신', 50000, '삼성전자', 10, 30000)
-kb.deposit(50000)
+kb = StockAccount('KB증권', '101-12-1222', '김유신', 100000, '삼성전자', 0, 0)
 kb.buy(3, 30000)
 kb.show()
-kb.sell(2, 35000)
+
+kb.sell(3, 40000)
 kb.show()
 
 iphone = SmartPhone('i8', '64GB', '128GB', '아이폰12', '010-1234-5678')
